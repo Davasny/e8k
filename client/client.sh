eae4a78d08d228c999d9a927bbcab0dc2d8c3410
Davasny <davasny@gmail.com>
2024-02-16 21:23:46 +0100

    feat: working dns server and client

---
+++ client/client.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+DOMAIN_NAME="domain.ltd"
+
+
+function main() {
+    # Check if a file path is provided
+    if [ -z "$1" ]; then
+        echo "Usage: $0 <file-path>"
+        exit 1
+    fi
+
+    # Assign the file path to a variable
+    FILE_PATH="$1"
+
+    tmp_file=$(mktemp)
+
+    # Compress the file, encode it to Base64, and then split into 32-character chunks
+    cat "$FILE_PATH" | gzip | xxd -p | fold -w63 > $tmp_file
+    LINES=$(wc -l $tmp_file)
+
+    # Initialize chunk counter
+    COUNTER=0
+
+    SESSION_ID=$(dig +short 0.$DOMAIN_NAME @127.0.0.1 -p 1053 | cut -d '.' -f 4)
+
+    # Read the split file line by line
+    while IFS= read -r line; do
+        echo "Part $COUNTER/$LINES"
+
+        # Construct the DNS query using the chunk and counter
+        QUERY="${line}.${COUNTER}.${SESSION_ID}.${DOMAIN_NAME}"
+
+        response=$(dig $QUERY +short @127.0.0.1 -p 1053)
+
+        # Increment the counter
+        ((COUNTER++))
+    done < $tmp_file
+
+    # Close session
+    dig +short 1.$SESSION_ID.$DOMAIN_NAME @127.0.0.1 -p 1053
+
+    rm $tmp_file
+}
+
+main "$@"
---
+++ server/server.ts
@@ -0,0 +1,117 @@
+import { Packet, createServer } from "dns2";
+
+import { existsSync, mkdirSync, writeFileSync } from "fs";
+import { join } from "path";
+import { gunzip } from "zlib";
+
+/**
+ * query: 0.domain.ltd - new session request
+ * response: 1.0.0.x - session number
+ * */
+
+/**
+ * query: 1.x.domain.ltd - finish session x
+ * response: 1.0.0.1 - session finished
+ * */
+
+/**
+ * query: <data>.y.x.domain.ltd - chunk y of data for session x
+ * response: 1.0.0.1 - data received
+ * */
+
+type transferredStrings = string[];
+const sessions = new Map<number, transferredStrings>();
+
+const downloadsDir = join(__dirname, "remoteFiles");
+if (!existsSync(downloadsDir)) {
+	mkdirSync(downloadsDir, { recursive: true });
+}
+
+const server = createServer({
+	udp: true,
+	handle: (request, send, rinfo) => {
+		console.log(`Received DNS request from ${rinfo.address}`);
+
+		const response = Packet.createResponseFromRequest(request);
+		const [question] = request.questions;
+		const { name } = question;
+
+		console.log(`Querying for name: ${name}`);
+
+		const labels = name.split(".");
+
+		let responseIp = "1.1.1.1";
+		if (labels.length === 3 && labels[0] === "0") {
+			console.log("New session request");
+			const existingSessions = Array.from(sessions.keys());
+			const sessionNumber = Math.max(...existingSessions, 0) + 1;
+			responseIp = `1.0.0.${sessionNumber}`;
+			sessions.set(sessionNumber, []);
+		}
+
+		if (labels.length === 4 && labels[0] === "1") {
+			console.log("Finish session request");
+			const sessionNumber = Number(labels[1]);
+			if (sessions.has(sessionNumber)) {
+				const session = sessions.get(sessionNumber);
+				const hexData = session.join("");
+
+				const buffer = Buffer.from(hexData, "hex");
+
+				gunzip(buffer, (err, decompressed) => {
+					if (err) {
+						console.error("Error decompressing data", err);
+						return;
+					}
+
+					const date = new Date();
+
+					// Construct the file path within the downloads directory
+					const filePath = join(
+						downloadsDir,
+						`session-${sessionNumber}-${date.toISOString()}.txt`,
+					);
+
+					// Write the decompressed data to file
+					writeFileSync(filePath, decompressed);
+					console.log(
+						`Session ${sessionNumber} data written to file: ${filePath}`,
+					);
+				});
+
+				sessions.delete(sessionNumber);
+				responseIp = "1.0.0.1";
+			} else {
+				// handle incorrect session number, probably do nothing
+			}
+		}
+
+		if (labels.length === 5 && labels[0] !== "") {
+			console.log("Data chunk received");
+
+			const sessionNumber = Number(labels[2]);
+			const chunkNumber = Number(labels[1]);
+
+			if (sessions.has(sessionNumber)) {
+				const session = sessions.get(sessionNumber);
+				session[chunkNumber] = labels[0];
+			}
+		}
+
+		response.answers.push({
+			name,
+			type: Packet.TYPE.A,
+			class: Packet.CLASS.IN,
+			ttl: 1,
+			address: responseIp,
+		});
+
+		send(response);
+	},
+});
+
+server.on("listening", () => console.log("DNS server listening on port 1053"));
+
+server.listen({
+	udp: 1053,
+});
