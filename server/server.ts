7a0b7bafc78228e81c73396baa8a083524ced071
Davasny <davasny@gmail.com>
2024-02-16 21:29:25 +0100

    style(server): add biome config

--- server/server.ts
+++ server/server.ts
@@ -24,94 +24,94 @@ const sessions = new Map<number, transferredStrings>();
 
 const downloadsDir = join(__dirname, "remoteFiles");
 if (!existsSync(downloadsDir)) {
-	mkdirSync(downloadsDir, { recursive: true });
+  mkdirSync(downloadsDir, { recursive: true });
 }
 
 const server = createServer({
-	udp: true,
-	handle: (request, send, rinfo) => {
-		console.log(`Received DNS request from ${rinfo.address}`);
+  udp: true,
+  handle: (request, send, rinfo) => {
+    console.log(`Received DNS request from ${rinfo.address}`);
 
-		const response = Packet.createResponseFromRequest(request);
-		const [question] = request.questions;
-		const { name } = question;
+    const response = Packet.createResponseFromRequest(request);
+    const [question] = request.questions;
+    const { name } = question;
 
-		console.log(`Querying for name: ${name}`);
+    console.log(`Querying for name: ${name}`);
 
-		const labels = name.split(".");
+    const labels = name.split(".");
 
-		let responseIp = "1.1.1.1";
-		if (labels.length === 3 && labels[0] === "0") {
-			console.log("New session request");
-			const existingSessions = Array.from(sessions.keys());
-			const sessionNumber = Math.max(...existingSessions, 0) + 1;
-			responseIp = `1.0.0.${sessionNumber}`;
-			sessions.set(sessionNumber, []);
-		}
+    let responseIp = "1.1.1.1";
+    if (labels.length === 3 && labels[0] === "0") {
+      console.log("New session request");
+      const existingSessions = Array.from(sessions.keys());
+      const sessionNumber = Math.max(...existingSessions, 0) + 1;
+      responseIp = `1.0.0.${sessionNumber}`;
+      sessions.set(sessionNumber, []);
+    }
 
-		if (labels.length === 4 && labels[0] === "1") {
-			console.log("Finish session request");
-			const sessionNumber = Number(labels[1]);
-			if (sessions.has(sessionNumber)) {
-				const session = sessions.get(sessionNumber);
-				const hexData = session.join("");
+    if (labels.length === 4 && labels[0] === "1") {
+      console.log("Finish session request");
+      const sessionNumber = Number(labels[1]);
+      if (sessions.has(sessionNumber)) {
+        const session = sessions.get(sessionNumber);
+        const hexData = session.join("");
 
-				const buffer = Buffer.from(hexData, "hex");
+        const buffer = Buffer.from(hexData, "hex");
 
-				gunzip(buffer, (err, decompressed) => {
-					if (err) {
-						console.error("Error decompressing data", err);
-						return;
-					}
+        gunzip(buffer, (err, decompressed) => {
+          if (err) {
+            console.error("Error decompressing data", err);
+            return;
+          }
 
-					const date = new Date();
+          const date = new Date();
 
-					// Construct the file path within the downloads directory
-					const filePath = join(
-						downloadsDir,
-						`session-${sessionNumber}-${date.toISOString()}.txt`,
-					);
+          // Construct the file path within the downloads directory
+          const filePath = join(
+            downloadsDir,
+            `session-${sessionNumber}-${date.toISOString()}.txt`,
+          );
 
-					// Write the decompressed data to file
-					writeFileSync(filePath, decompressed);
-					console.log(
-						`Session ${sessionNumber} data written to file: ${filePath}`,
-					);
-				});
+          // Write the decompressed data to file
+          writeFileSync(filePath, decompressed);
+          console.log(
+            `Session ${sessionNumber} data written to file: ${filePath}`,
+          );
+        });
 
-				sessions.delete(sessionNumber);
-				responseIp = "1.0.0.1";
-			} else {
-				// handle incorrect session number, probably do nothing
-			}
-		}
+        sessions.delete(sessionNumber);
+        responseIp = "1.0.0.1";
+      } else {
+        // handle incorrect session number, probably do nothing
+      }
+    }
 
-		if (labels.length === 5 && labels[0] !== "") {
-			console.log("Data chunk received");
+    if (labels.length === 5 && labels[0] !== "") {
+      console.log("Data chunk received");
 
-			const sessionNumber = Number(labels[2]);
-			const chunkNumber = Number(labels[1]);
+      const sessionNumber = Number(labels[2]);
+      const chunkNumber = Number(labels[1]);
 
-			if (sessions.has(sessionNumber)) {
-				const session = sessions.get(sessionNumber);
-				session[chunkNumber] = labels[0];
-			}
-		}
+      if (sessions.has(sessionNumber)) {
+        const session = sessions.get(sessionNumber);
+        session[chunkNumber] = labels[0];
+      }
+    }
 
-		response.answers.push({
-			name,
-			type: Packet.TYPE.A,
-			class: Packet.CLASS.IN,
-			ttl: 1,
-			address: responseIp,
-		});
+    response.answers.push({
+      name,
+      type: Packet.TYPE.A,
+      class: Packet.CLASS.IN,
+      ttl: 1,
+      address: responseIp,
+    });
 
-		send(response);
-	},
+    send(response);
+  },
 });
 
 server.on("listening", () => console.log("DNS server listening on port 1053"));
 
 server.listen({
-	udp: 1053,
+  udp: 1053,
 });
